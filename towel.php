1ba1f0b1f494b7884d49807aeda9aa9dd2406517
JagerSprinkles <Jager.Sprinkles@gmail.com>
2016-03-25 17:21:52 -0400

    Update towel.php

--- towel.php
+++ towel.php
@@ -5,7 +5,7 @@ if (!empty($_POST)) {
 	$message = "";
 	$_POST['wat'] .= "\r\n";
 	
-	$message .= $_POST['mike'] . $_POST['vince'] . $_POST['marc'] . $_POST['warr'] . $_POST['neil'] . $_POST['alex'] . $_POST['nick'] . $_POST['wat'] ;
+	$message .= $_POST['mike'] . $_POST['vince'] . $_POST['marc'] . $_POST['warr'] . $_POST['neil'] . $_POST['alex'] . $_POST['christian'] . $_POST['nick'] . $_POST['wat'] ;
 	
 	$message .= $_POST['exx'];
 
@@ -17,7 +17,7 @@ if (!empty($_POST)) {
 	
 	if($_POST['vinceS']) mail("qqqqqqqqq","", $message, "From:qqqqqqqqqqq");
 	if($_POST['mikeS']) mail("qqqqqqqqqqqqq","", $message, "From:qqqqqqqqqqq");
-	
+	if($_POST['chriS']) mail("qqqqqqqqqqqqq","", $message, "From:qqqqqqqqqqq");
 	
 }
 ?>
